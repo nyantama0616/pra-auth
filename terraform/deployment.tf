0cb53e80ee390da106f8b8ef7e8333119b488be4
nyantama0616 <g2124029@fun.ac.jp>
2025-06-01 00:17:11 +0900

    adminも作成した

--- terraform/deployment.tf
+++ terraform/deployment.tf
@@ -3,3 +3,9 @@ resource "vercel_deployment" "pra_auth_web" {
   ref        = "main"
   production  = true
 }
+
+resource "vercel_deployment" "pra_auth_admin" {
+  project_id  = vercel_project.pra_auth_admin.id
+  ref        = "main"
+  production  = true
+}
--- terraform/project.tf
+++ terraform/project.tf
@@ -9,3 +9,15 @@ resource "vercel_project" "pra_auth_web" {
 
   root_directory = "frontend/apps/web"
 }
+
+resource "vercel_project" "pra_auth_admin" {
+  name      = "pra-auth-admin"
+  framework = "nextjs"
+
+  git_repository = {
+    type = "github"
+    repo = "nyantama0616/pra-auth"
+  }
+
+  root_directory = "frontend/apps/admin"
+}
